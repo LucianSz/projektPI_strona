f2c5a226c791381f44fe189e7a22b0bb58b48323
LucianSz <90635362+LucianSz@users.noreply.github.com>
2023-01-06 20:32:17 +0100

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $connect = mysqli_connect("localhost", "root","", "strona_internetowa");
-$host = "localhost";
-$db_user = "root";
-$db_password = "";
-$db_name = "strona_internetowa";
-
-?>
